c279215817887c952f1900106aad09a3bc5fd9ea
dhivaharjey <dhivahar19399@gmail.com>
2024-02-18 20:11:42 +0530

    card style

--- src/App.jsx
+++ src/App.jsx
@@ -73,25 +73,21 @@ function App() {
       details: "SIZES: M , L  , XL",
     },
   ];
+
   return (
     <div>
       <NavBar cartcount={cartcount} />
       <Header />
-      <section className="py-5">
-        <div className="container px-4 px-lg-5 mt-5">
-          <div className="row gx-4 gx-lg-5 row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 justify-content-center">
-            {productCard.map((item, index) => {
-              return (
-                <Products
-                  item={item}
-                  index={index}
-                  setCartCount={setCartCount}
-                />
-              );
-            })}
-          </div>
+
+      <div className="container px-4 px-lg-5 mt-5">
+        <div className="row gx-4 gx-lg-5 row-cols-md-2 ">
+          {productCard.map((item, index) => {
+            return (
+              <Products item={item} index={index} setCartCount={setCartCount} />
+            );
+          })}
         </div>
-      </section>
+      </div>
 
       <Footer />
     </div>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -12,59 +12,52 @@ function Products({ item, index, setCartCount }) {
     setCartCount((val) => val - 1);
   };
   return (
-    <div key={index}>
-      <div className="col  mb-5">
-        <div className="card h-200">
-          {/* <!-- Product image--> */}
-          <img
-            className="card-img-top object-fit-cover border rounded"
-            src={item.image}
-            alt="product image"
-          />
-          {/* <!-- Product details--> */}
-          <div className="card-body p-5 mb-0">
-            <div className="text-center">
-              {/* <!-- Product name--> */}
-              <h5 className="fw-bolder">{item.productName}</h5>
+    <div className="col-sm-12 col-md-6 col-lg-3 mb-5 d-flex align-items-stretch">
+      <div className="card mb-5">
+        {/* <!-- Product image--> */}
+        <img
+          className="card-img-top object-fit-cover border rounded"
+          src={item.image}
+          alt="product image"
+        />
+        {/* <!-- Product details--> */}
+        <div className="card-body d-flex flex-column">
+          <div className="text-center">
+            {/* <!-- Product name--> */}
+            <h5 className="fw-bolder">{item.productName}</h5>
 
-              {/* <!-- Product price--> */}
-              <div class="text-dark fs-5 text-decoration-line-through">
-                {item.oldPrice}
-              </div>
-              <span className="text-success fs-6 fw-bolder  fst-italic">
-                {item.offers}
-              </span>
-              <div class="text-dark fs-4 badge bg-normal text-wrap mb-4">
-                &#8377;{item.price}
-              </div>
+            {/* <!-- Product price--> */}
+            <div class="text-dark fs-5 text-decoration-line-through">
+              {item.oldPrice}
+            </div>
+            <span className="text-success fs-6 fw-bolder  fst-italic">
+              {item.offers}
+            </span>
+            <div class="text-dark fs-4 badge bg-normal text-wrap mb-4">
+              &#8377;{item.price}
+            </div>
 
-              {/* Description */}
-              <div class="text-dark ">
-                <h6 className="text-left font-monospace text-wrap ">
-                  Details:
-                </h6>
-                <p className="font-monospace fst-italic text-wrap">
-                  {item.details}
-                </p>
-              </div>
+            {/* Description */}
+            <div class="text-dark ">
+              <h6 className="text-left font-monospace text-wrap ">Details:</h6>
+              <p className="font-monospace fst-italic text-wrap">
+                {item.details}
+              </p>
             </div>
           </div>
-          {/* <!-- Product actions--> */}
-          <div className="card-footer p-4  pt-0 border-top-0 bg-transparent">
-            <div className="text-center " key={index}>
-              {status ? (
-                <button className="btn btn-outline-primary " onClick={addCart}>
-                  Add to cart
-                </button>
-              ) : (
-                <button
-                  className="btn btn-outline-danger "
-                  onClick={removeCart}
-                >
-                  Remove from cart
-                </button>
-              )}
-            </div>
+        </div>
+        {/* <!-- Product actions--> */}
+        <div className="card-footer p-4  pt-0 border-top-0 bg-transparent">
+          <div className="text-center " key={index}>
+            {status ? (
+              <button className="btn btn-outline-primary " onClick={addCart}>
+                Add to cart
+              </button>
+            ) : (
+              <button className="btn btn-outline-danger " onClick={removeCart}>
+                Remove from cart
+              </button>
+            )}
           </div>
         </div>
       </div>
